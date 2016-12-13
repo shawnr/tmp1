baec12c218b13b300de883f48a7475d3bc3d58df
Shawn Rider <shawn@shawnrider.com>
2016-12-13 15:37:41 -0800

    Initial commit.
    Change-Id: I7f8586b16e5acd8b16767a1f6f78e9d1ae613d06

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hashbangApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the hashbangApp
+ */
+angular.module('hashbangApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hashbangApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hashbangApp
+ */
+angular.module('hashbangApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
